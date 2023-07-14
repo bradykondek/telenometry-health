6e4375dd5a3c8d9ebed578c6c295d62b4d2c3bda
Brady Kondek <34287058+bradykondek@users.noreply.github.com>
2023-07-14 11:16:40 -0400

    Add files via upload

---
+++ smooth-nav.js
@@ -0,0 +1,30 @@
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+
+mainNavLinks.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    let target = document.querySelector(event.target.hash);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
